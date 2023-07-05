f66d3b24c6760ad805ec1b7330e40a5a53166946
Relativiteit <alejocain@gmail.com>
2023-07-05 21:41:04 +0200

    added ssh_key resource

--- main.tf
+++ main.tf
@@ -13,10 +13,23 @@ provider "digitalocean"{}
 # or using -var="do_token=..." CLI option
 # variable "do_token" {
 
+resource "digitalocean_ssh_key" "web" {
+  name       = "web app SHH key"
+  public_key = file("${path.module}/files/id_rsa.pub")
+}
+
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-20-04-x64"
   name   = "testing-1"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  monitoring = true
+  private_networking = false
+  ssh_keys = [digitalocean_ssh_key.web.id]
+  user_data = // to install packages like doctl
 }
+
+
+
+
