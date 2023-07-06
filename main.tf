978665db52e005c0dbf2cdd172328e84bf6d4f90
Relativiteit <alejocain@gmail.com>
2023-07-06 15:25:48 +0200

    added multiple servers and loadbalancer

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+
 # Set the variable value in *.tfvars file 
 # or using -var="do_token=..." CLI option
 # variable "do_token" {
@@ -24,14 +25,52 @@ resource "digitalocean_ssh_key" "web" {
 
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "web" {
+  count      = 2
   image      = "ubuntu-20-04-x64"
-  name       = "testing-1"
+  name       = "web-${count.index}"
   region     = "ams3"
   size       = "s-1vcpu-1gb"
   monitoring = true
   #private_networking = false // need to use vpc_uuid instead since private_networking is deprecated
   ssh_keys  = [digitalocean_ssh_key.web.id]
   user_data = file("${path.module}/files/user-data.sh")
+}
+
+
+
+resource "digitalocean_loadbalancer" "web" {
+  name   = "web-lb"
+  region = "ams3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 8080
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 8080
+    protocol = "http"
+    path = "/"
+  }
+
+  droplet_ids = digitalocean_droplet.web.*.id
+}
+
+# Create a new domain
+resource "digitalocean_domain" "domain" {
+  name = "goat.kantorobotics.jp"
+  # ip_address = digitalocean_droplet.web.ipv4_address
+}
+
+# Add an A record to the domain for www.example.com.
+resource "digitalocean_record" "main" {
+  domain = digitalocean_domain.domain.name
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_loadbalancer.web.ip
 }
 
 
